4efcee764f6b94a9d58440f14a0f965430f98c68
kleintonno <116087195+kleintonno@users.noreply.github.com>
2024-05-07 10:06:59 +0200

    Update setup_kiosk.sh

--- setup_kiosk.sh
+++ setup_kiosk.sh
@@ -7,26 +7,21 @@ sudo apt update
 sudo apt upgrade -y
 
 # Step 2: Install required packages
-sudo apt install chromium-browser sed xdotool unclutter -y
+sudo apt install chromium-browser sed unclutter -y
 
-# Step 3: Create a user called "kiosk"
-sudo useradd -m kiosk
-
-# Step 4: Enable automatic login and disable Wayland
-sudo sed -i 's/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/' /etc/gdm3/custom.conf
-sudo sed -i 's/#  AutomaticLogin = user1/AutomaticLogin = kiosk/' /etc/gdm3/custom.conf
+# Step 3: Disable Wayland
 sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
 
-# Step 5: Create cleanup script
+# Step 4: Create cleanup script
 sudo mkdir -p /opt/kiosk/
 sudo tee /opt/kiosk/cleanup_kiosk.sh > /dev/null << EOF
 #!/bin/bash
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/dminti/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/dminti/.config/chromium/Default/Preferences
 EOF
 sudo chmod +x /opt/kiosk/cleanup_kiosk.sh
 
-# Step 6: Create systemd service
+# Step 5: Create systemd service
 sudo tee /lib/systemd/system/kiosk.service > /dev/null << EOF
 [Unit]
 Description=Chromium Kiosk
@@ -37,16 +32,16 @@ After=graphical.target
 Environment=DISPLAY=:0
 Type=simple
 ExecStartPre=/opt/kiosk/cleanup_kiosk.sh
-ExecStart=/usr/bin/chromium-browser -noerrdialogs --disable-infobars --no-first-run --start-maximized --kiosk $WEB_APP_URL
+ExecStart=/usr/bin/chromium-browser -noerrdialogs --no-sandbox --disable-infobars --no-first-run --start-maximized --kiosk $WEB_APP_URL
 Restart=always
-User=kiosk
-Group=kiosk
+User=dminti
+Group=dminti
 
 [Install]
 WantedBy=graphical.target
 EOF
 
-# Step 7: Enable the kiosk service
+# Step 6: Enable the kiosk service
 sudo systemctl enable kiosk
 
 echo "Setup complete. Rebooting now..."
