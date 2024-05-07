fd63abc95e5767ae78bb2d9cfde0a0e3bd1654c9
kleintonno <116087195+kleintonno@users.noreply.github.com>
2024-05-07 12:53:56 +0200

    Update setup_kiosk.sh

--- setup_kiosk.sh
+++ setup_kiosk.sh
@@ -1,53 +1,156 @@
 #!/bin/bash
 
+launch_dir=`pwd`
+readonly START_TIME=`date +%Y-%m-%dT%H:%M:%S`
+readonly LOG_DIR="logs"
+readonly LOG_FILE="build_$START_TIME.log"
+readonly LOG_OUT="$launch_dir/$LOG_DIR/$LOG_FILE"
 readonly WEB_APP_URL="https://dminti.deploy.qwellco.de"
 
-# Step 1: Update Ubuntu
-sudo apt update
-sudo apt upgrade -y
+readonly KIOSK_DESKTOP_RC="\
+[Desktop]\n\
+Session=kiosk\n"
 
-# Step 2: Install SSH
-sudo apt install openssh-server
-sudo systemctl enable ssh
-sudo systemctl start ssh
+readonly KIOSK_AUTOLOGIN="\
+[Seat:*]\n\
+allow-guest=false\n\
+greeter-hide-users=true\n\
+autologin-guest=false\n\
+autologin-user=kiosk\n\
+autologin-user-timeout=0\n"
 
-# Step 3: Install required packages
-sudo apt install chromium-browser sed unclutter -y
+readonly KIOSK_DEFAULT_SESSION="\
+[Seat:*]\n\
+user-session=kiosk\n"
 
-# Step 4: Disable Wayland
-sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+readonly KIOSK_XSESSION="\
+[Desktop Entry]\n\
+Type=Application\n\
+Encoding=UTF-8\n\
+Name=Kiosk\n\
+Comment=Start a Chrome-based Kiosk session\n\
+Exec=/bin/bash /home/kiosk/start-chrome.sh\n\
+Icon=google=chrome"
 
-# Step 5: Create cleanup script
-sudo mkdir -p /opt/kiosk/
-sudo tee /opt/kiosk/cleanup_kiosk.sh > /dev/null << EOF
-#!/bin/bash
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/dminti/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/dminti/.config/chromium/Default/Preferences
-EOF
-sudo chmod +x /opt/kiosk/cleanup_kiosk.sh
+readonly START_CHROME="\
+#!/bin/bash\n\n\
+X_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$1 }' | sed 's/[^0-9]*//g'\`\n\
+Y_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$2 }' | awk '{ print \$1 }'\`\n\n\
+/usr/bin/google-chrome --kiosk --start-fullscreen --window-position=0,0 \
+--window-size=\$X_RES,\$Y_RES --no-first-run --incognito --no-default-browser-check \
+--disable-translate $WEB_APP_URL\n"
 
-# Step 6: Create systemd service
-sudo tee /lib/systemd/system/kiosk.service > /dev/null << EOF
-[Unit]
-Description=Chromium Kiosk
-Wants=graphical.target
-After=graphical.target
+###############################################################################
 
-[Service]
-Environment=DISPLAY=:0
-Type=simple
-ExecStartPre=/opt/kiosk/cleanup_kiosk.sh
-ExecStart=/usr/bin/chromium-browser -noerrdialogs --no-sandbox --disable-infobars --no-first-run --start-maximized --kiosk $WEB_APP_URL
-Restart=always
-User=dminti
-Group=dminti
+# Configuration steps
+do_install_chrome=y
+do_create_kiosk_user=y
+do_create_kiosk_xsession=y
+do_enable_kiosk_autologin=y
+do_write_chrome_startup=y
 
-[Install]
-WantedBy=graphical.target
-EOF
+###############################################################################
 
-# Step 7: Enable the kiosk service
-sudo systemctl enable kiosk
+msg() {
+    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)]: $@" >&2
+}
 
-echo "Setup complete. Rebooting now..."
+###############################################################################
+
+create_kiosk_user() {
+    msg "Creating kiosk group and user"
+    getent group kiosk || (
+        groupadd kiosk
+        useradd kiosk -s /bin/bash -m -g kiosk -p '*'
+        passwd -d kiosk # Delete kiosk's password
+        # Lock kiosk's account so that kiosk can't login using SSH or by
+        # switching tty. However, lightdm can still start a session with this
+        # user
+        passwd -l kiosk
+    )
+}
+
+###############################################################################
+
+create_kiosk_xsession() {
+    msg "Creating Kiosk Xsession"
+    echo -e $KIOSK_XSESSION > /usr/share/xsessions/kiosk.desktop
+}
+
+###############################################################################
+
+install_chrome() {
+    msg "Installing Chrome browser"
+    grep chrome /etc/apt/sources.list.d/google-chrome.list >&/dev/null || (
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+        echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+        apt-get update
+        apt-get install -y --no-install-recommends google-chrome-stable
+    )
+}
+
+###############################################################################
+
+enable_kiosk_autologin() {
+    msg "Enabling Kiosk autologin"
+    echo -e $KIOSK_AUTOLOGIN > /etc/lightdm/lightdm.conf
+    echo -e $KIOSK_DEFAULT_SESSION > /etc/lightdm/lightdm.conf.d/99-kiosk.conf
+}
+
+###############################################################################
+
+write_chrome_startup() {
+    msg "Writing script which starts Chrome with dynamic window size"
+    echo -e $START_CHROME > /home/kiosk/start-chrome.sh
+    chown kiosk:kiosk /home/kiosk/start-chrome.sh
+    chmod +x /home/kiosk/start-chrome.sh
+}
+
+###############################################################################
+# Start execution
+###############################################################################
+
+# Provide an opportunity to stop installation
+msg "Configure Kiosk"
+read -p "Press ENTER to continue (c to cancel) ..." entry
+if [ ! -z $entry ]; then
+    if [ $entry = "c" ]; then
+        msg "Install cancelled"
+        exit 0
+    fi
+fi
+
+if [ $do_install_chrome = "y" ]; then
+    install_chrome
+fi
+
+if [ $do_create_kiosk_user = "y" ]; then
+    create_kiosk_user
+fi
+
+if [ $do_create_kiosk_xsession = "y" ]; then
+    create_kiosk_xsession
+fi
+
+if [ $do_enable_kiosk_autologin = "y" ]; then
+    enable_kiosk_autologin
+fi
+
+if [ $do_write_chrome_startup = "y" ]; then
+    write_chrome_startup
+fi
+
+msg "Installation complete, press ENTER to reboot!"
+if [ ! -z $entry ]; then
+    if [ $entry = "c" ]; then
+        msg "Reboot cancelled"
+        exit 0
+    fi
+fi
 sudo reboot
+
+exit 0
+
+###############################################################################
+# End execution
+###############################################################################
