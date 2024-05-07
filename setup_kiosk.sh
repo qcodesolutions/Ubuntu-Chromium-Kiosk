2a9d5ada5ab6abf512c2dff1a6d0043d2bd1a846
kleintonno <116087195+kleintonno@users.noreply.github.com>
2024-05-07 18:07:15 +0200

    Update setup_kiosk.sh

--- setup_kiosk.sh
+++ setup_kiosk.sh
@@ -36,8 +36,8 @@ readonly START_CHROME="\
 #!/bin/bash\n\n\
 X_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$1 }' | sed 's/[^0-9]*//g'\`\n\
 Y_RES=\`xrandr | grep \"*\" | awk -Fx '{ print \$2 }' | awk '{ print \$1 }'\`\n\n\
-/usr/bin/google-chrome --kiosk --start-fullscreen --window-position=0,0 \
---window-size=\$X_RES,\$Y_RES --no-first-run --incognito --no-default-browser-check \
+/usr/bin/chromium --kiosk --start-fullscreen --window-position=0,0 \
+--window-size=\$X_RES,\$Y_RES --no-first-run --no-default-browser-check \
 --disable-translate $WEB_APP_URL\n"
 
 ###############################################################################
